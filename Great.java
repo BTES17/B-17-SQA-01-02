d03cfc8478b7f6619949dff4b0af5fe4561e320e
BTES17 <ramandeep@btes.co.in>
2017-03-28 23:10:27 +0530

    Greatest of three nos. if & else if condition - Rahul Mehta
    /**
     * Created by RahulMehta on 3/28/2017.
     */
    public class Great {
    
    
        public void grea()
    
        {
    int a,b,c;
    a=50;
    b=50;
    c=50;
    
    if(a>b&&a>c)
    {
        System.out.println("a is the greatest number");
    }
    
    else if (b>c)
    {
        System.out.println("b is the greatest number");
    }
    
    else if (c>b)
    
    {
        System.out.println("c is the greatest number");
    }
    
    
    else if (a==b&&b==c)
    
    {
        System.out.println("All are same");
    
    }
        }
    
    public static void main(String args[])
    
        {
          Great Obj=new Great();
          Obj.grea();
    
        }
    
    
    }

--- Great.java
+++ Great.java
@@ -1,50 +1,49 @@
+/**
+ * Created by RahulMehta on 3/28/2017.
+ */
+public class Great {
 
-public class Great 
+
+    public void grea()
+
+    {
+int a,b,c;
+a=50;
+b=50;
+c=50;
+
+if(a>b&&a>c)
+{
+    System.out.println("a is the greatest number");
+}
+
+else if (b>c)
+{
+    System.out.println("b is the greatest number");
+}
+
+else if (c>b)
 
 {
+    System.out.println("c is the greatest number");
+}
 
-	public void three()
-	
-	{
-		int a,b,c;
-		a=5;
-		b=7;
-		c=4;
-		
-		if (a>b)
-		{
-			
-			if (b>c)
-				
-			{
-				
-				System.out.println("a is the greatest number");
-			}
-			
-		}
-		
-		else
-		{
-			if(b>c)
-			{
-				System.out.println("b is the greatest number");
-			}
-			else
-			{
-				System.out.println("c is greatest number");
-				
-			}
-		}
-	}
-		
-		public static void main(String args[])
-		
-		{
-			
-			Great obj=new Great();
-			obj.three();
-			
-		}
-		
-	
+
+else if (a==b&&b==c)
+
+{
+    System.out.println("All are same");
+
+}
+    }
+
+public static void main(String args[])
+
+    {
+      Great Obj=new Great();
+      Obj.grea();
+
+    }
+
+
 }
