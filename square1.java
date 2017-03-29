0034d3d43838a0ffb588e5d3726b4e258bf4d0a1
BTES17 <ramandeep@btes.co.in>
2017-03-29 23:01:10 +0530

    ujval

---
+++ square1.java
@@ -0,0 +1,27 @@
+
+
+ import java.util.Scanner;
+ public class square1 {
+ 
+	    public static void main(String[] args)
+			{
+				int n;
+				Scanner scan=new Scanner(System.in);
+				System.out.println("Enter number");
+				n=scan.nextInt();
+				
+				int i=1;
+				while(i<=n)
+				{
+					
+					
+					int a;
+					System.out.println(i*i);
+			
+					i++;
+				}
+				
+				
+			}
+		}
+
