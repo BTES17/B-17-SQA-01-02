50b8f8960273ebf2fa7da9d71f876518d5f322dc
BTES17 <ramandeep@btes.co.in>
2017-03-29 11:13:46 -0700

    Square of numbers 'for loop' 29 March- Rahul Mehta
    /**
     * Created by RahulMehta on 3/29/2017.
     *  WAP to enter the value in integer N through keyborad and print the series of the square of natural number upto N number of elements. like if you have enter 5 then square of first five natural number should be printed as 1 4 9 16 25.
    
     */
    import java.util.Scanner;
    public class Square {
    
        public void num()
        {
            int a,i,n,b;
            System.out.println("Enter the number for which square is to be calculated");
            Scanner scan=new Scanner(System.in);
            a = scan.nextInt();
            for (i=1;i<=a;i++)
            {
                b=i*i;
                System.out.println("The square of subsequent number is "+b);
    
            }
    
    
    
        }
    
        public static void main(String args[]) {
            Square Obj = new Square();
            Obj.num();
        }
    
    }

--- Square.java
+++ Square.java
@@ -1,26 +1,31 @@
-//WAP to enter the value in integer N through keyborad and print the series of the 
- //square of natural number upto N number of elements. like if you have enter 5 then square
- //of first five natural number should be printed as 1 4 9 16 25.
+/**
+ * Created by RahulMehta on 3/29/2017.
+ *  WAP to enter the value in integer N through keyborad and print the series of the square of natural number upto N number of elements. like if you have enter 5 then square of first five natural number should be printed as 1 4 9 16 25.
 
-    import java.util.Scanner;
-	public class Square {
-    public static void main(String[] args)
-		{
-			int n;
-			Scanner scan=new Scanner(System.in);
-			System.out.println("Enter number");
-			n=scan.nextInt();
-			
-			int i=1;
-			while(i<=n)
-			{
-				
-				
-				System.out.println(i*i);
-				i++;
-			}
-			
-			
-		}
-	}
+ */
+import java.util.Scanner;
+public class Square {
 
+    public void num()
+    {
+        int a,i,n,b;
+        System.out.println("Enter the number for which square is to be calculated");
+        Scanner scan=new Scanner(System.in);
+        a = scan.nextInt();
+        for (i=1;i<=a;i++)
+        {
+            b=i*i;
+            System.out.println("The square of subsequent number is "+b);
+
+        }
+
+
+
+    }
+
+    public static void main(String args[]) {
+        Square Obj = new Square();
+        Obj.num();
+    }
+
+}
