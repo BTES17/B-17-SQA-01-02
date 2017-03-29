09a942f9ce8e0fe5f8a3f4670a078886825ca03c
BTES17 <ramandeep@btes.co.in>
2017-03-29 17:44:57 +0530

    Ujval

--- Greatest.java
+++ Greatest.java
@@ -1,61 +1,68 @@
-package Roman;
 
 public class Greatest {
-
-	void grt()
+void great()
+{
+	int a=10000;   //find greatest number among three using if and else statement
+	int b=900;
+	int c=555;
+	if (a>b && a>c)
 	{
-		int a=4; int b=7; int c=8;
-		
-		if (a>b && a>c)
-		{
-			System.out.println("a is greater" );
-		}
-	
-		else if (b>c)
+		System.out.println("a is greater");
+	}
+	else
+	{
+		if (b>c)
 		{
 			System.out.println("b is greater");
 		}
-		else if (c>b)
+		else 
 		{
-			System.out.println("c is greater");
+			
+				System.out.println("c is greater");
+			
 		}
-		
+	}
+}
+void great1()// find greatest number amongest three using conditional operator such as ?,:
+{
+	int a=100;
+	int b=20;
+	int c=30;
+	int t;
+  t= c>(a>b? a:b)? c:((a>b)?a:b); 
+  System.out.println(t);
+}
+void great2()//find greatest among three numner using if and else if statemnent
+{
+	int a=5;
+	int b=307;
+	int c=50;
+	if (a>b && a>c)
+	{
+		System.out.println("a is greater");
+	}
+	else if (b>c)
+	{
+		System.out.println("b is gretest");
 	}
 	
-	void grt1()
+	else
 	{
-		int a=9; int b=14; int c=12;
-		
-		if (a>b && a>c)
-		{
-
-			System.out.println("a is greater");
-		}
-		
-		else  
-		{
-			if (b>c)
-			{
-				System.out.println("b is greater");
-			}
-			
-			else
-			{
-			System.out.println("c is greater");
-		}
-		}
-		}
-		
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Greatest object=new Greatest();
-		object.grt();
-		Greatest object1=new Greatest();
-		object1.grt1();
-		
-
+		System.out.println("c is greatest");
 	}
+}
+	
+
+public static void main (String args[])
+{
+	Greatest obj=new Greatest();
+	Greatest obj1=new Greatest();
+	Greatest obj2=new Greatest();
+	obj.great();
+	obj1.great1();
+	obj2.great2();
+	
+}
+
 
 }
