c55eff43067066aefb599b0432a074e205ee2c26
BTES17 <ramandeep@btes.co.in>
2017-03-29 09:48:41 +0530

    Add files via upload
    calculator

---
+++ ishaan3.java
@@ -0,0 +1,36 @@
+package ishaan;
+public class ishaan3
+{
+		public static void main(String[] args) 
+		{
+			int z=2,a=10,b=8;
+			switch(z)
+			{
+			case 1:
+			{
+			System.out.println(a+b);
+			break;
+			}
+			case 2:
+			{
+			System.out.println(a-b);
+			break;
+			}
+			case 3:
+			{
+			System.out.println(a*b);
+			break;	
+			}
+			case 4:
+			{
+			System.out.println(a/b);
+			break;
+			}
+				default:
+			{
+				System.out.println("");
+			}
+			
+			}
+		}
+}
