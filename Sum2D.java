194b4649124e198726449aeac4277be23fb32fa5
BTES17 <ramandeep@btes.co.in>
2017-04-04 09:50:40 +0530

    Add files via upload
    sum of 2D array by ISHAAN

---
+++ Sum2D.java
@@ -0,0 +1,26 @@
+package ishaan;
+
+import java.util.Scanner;
+
+public class Sum2D 
+{
+	public static void main(String[] args)
+	{ 
+		int r,c=0,sum=0;
+		Scanner scan=new Scanner (System.in);
+		System.out.println("Enter the size of array");
+		int rowsize=scan.nextInt();
+		int columnsize=scan.nextInt();
+		int[][] array=new int[rowsize][columnsize];
+		
+		for(r=0;r<rowsize;r++)
+		{
+			for(c=0;c<columnsize;c++)
+			{
+				array[r][c]=scan.nextInt();
+				sum=array[r][c]+sum;
+			}
+		}
+		System.out.println(sum);
+}
+}
