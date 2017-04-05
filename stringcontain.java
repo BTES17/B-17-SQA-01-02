5d13dbfd84e3d4813daee0f23058f30974f9b5cb
BTES17 <ramandeep@btes.co.in>
2017-04-05 22:11:06 +0530

    Ujval (5/4/2017)

---
+++ stringcontain.java
@@ -0,0 +1,10 @@
+//Wap to find that a word exist in the String or not. 
+public class stringcontain {
+	public static void main(String args[])
+	{
+		String s1="Do you know what is Java"; 
+        System.out.println(s1.contains("Java"));
+	}
+}
+
+
