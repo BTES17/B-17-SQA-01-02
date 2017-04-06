c76baae78baaa6390413d95ce445ee6bf75adaec
BTES17 <ramandeep@btes.co.in>
2017-04-06 09:21:49 +0530

    Add files via upload

---
+++ ujval/stringReverse.java
@@ -0,0 +1,20 @@
+//WAP to reverse a String without using the Reverse method.
+public class stringReverse {
+	public static void main(String args[]){  
+		
+		{
+			String s1= "Hello Java";
+			for(int i= s1.length()-1;i>=0;--i)
+			{
+			
+			
+			System.out.print(s1.charAt(i)); 
+		
+			
+			
+			
+				
+			}
+		}
+	}
+}
