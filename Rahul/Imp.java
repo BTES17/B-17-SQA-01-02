15ab2c1ee46f23d4eab07b4ee563fe09cfcc7e6a
BTES17 <ramandeep@btes.co.in>
2017-04-12 10:01:12 +0530

    Static Import Example
    import static java.lang.System.out;
    import static java.lang.Math.*;
    public class Imp {
    
    	static void Imp1() {
    		int a=25;
    		double c=sqrt(a);
    		out.println("This is the use of static method and static import");
    		out.println(c);
    	}
    
    	public static void main(String args[])
    	{
    		
    		Imp1();
    	}
    	
    }

---
+++ Rahul/Imp.java
@@ -0,0 +1,18 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class Imp {
+
+	static void Imp1() {
+		int a=25;
+		double c=sqrt(a);
+		out.println("This is the use of static method and static import");
+		out.println(c);
+	}
+
+	public static void main(String args[])
+	{
+		
+		Imp1();
+	}
+	
+}
