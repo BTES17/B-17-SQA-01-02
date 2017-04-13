81c525e1f5033eee5e1df4eb67fc86fc82791188
BTES17 <ramandeep@btes.co.in>
2017-04-13 09:09:49 +0530

    Classes in different package with import
    package pack1;
    public class Info {
    
    	public void Hello() {
    		
    		System.out.println("This package only contains the value,the other package would be used to call this package.");
    	}
    
    	
    }
    
    
    //2nd source code
    
    package pack2;
    import pack1.Info;
    
    public class Print {
    
    	public void printt() {
    	
    		System.out.println("This is running successfuly");
    		
    			}
    	public static void main(String args[])
    	{
    	Print Obj=new Print();
    	Info Obj1=new Info();
    	Obj1.Hello();
    	Obj.printt();
    	
    	}
    }

---
+++ Rahul/Info.java
@@ -0,0 +1,11 @@
+
+package pack1;
+public class Info {
+
+	public void Hello() {
+		
+		System.out.println("This package only contains the value,the other package would be used to call this package.");
+	}
+
+	
+}
---
+++ Rahul/Print.java
@@ -0,0 +1,19 @@
+package pack2;
+import pack1.Info;
+
+public class Print {
+
+	public void printt() {
+	
+		System.out.println("This is running successfuly");
+		
+			}
+	public static void main(String args[])
+	{
+	Print Obj=new Print();
+	Info Obj1=new Info();
+	Obj1.Hello();
+	Obj.printt();
+	
+	}
+}
