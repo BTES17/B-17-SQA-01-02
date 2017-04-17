1cde6f5db90a3461b884f82a6dbf2a828c829f96
BTES17 <ramandeep@btes.co.in>
2017-04-17 12:13:43 +0530

    using Final Keyword (17/4/2017)

---
+++ Pooja/Parent.java
@@ -0,0 +1,15 @@
+//Example of final variable
+package MyPack;
+
+public class Parent {
+	
+	  
+		 final int a=10;//final variable  
+		 void abc(){  
+		  a=50;  
+		 }  
+		 public static void main(String args[]){  
+		 Parent obj=new  Parent();  
+		 obj.abc();  
+		 }  
+		}
