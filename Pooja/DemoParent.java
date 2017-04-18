a31454b1917ece8ff56e7b37bc804c99306f9f9f
BTES17 <ramandeep@btes.co.in>
2017-04-18 18:06:18 +0530

    interface (18/4/2017)

---
+++ Pooja/DemoParent.java
@@ -0,0 +1,19 @@
+package MyPack1;
+
+public interface DemoParent {
+	abstract void abc1();
+	 void abc2();
+	 static void abc()
+	 {
+		 System.out.println("output using static method");
+	 }
+	 public static void main(String args[])
+	 {
+		 abc();
+	 }
+	 
+	 
+	
+	
+
+}
