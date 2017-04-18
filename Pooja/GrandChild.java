69d0ed9945bd1d72155b2fa545b1421065795ef1
BTES17 <ramandeep@btes.co.in>
2017-04-18 18:10:30 +0530

    multilevel inheritance

---
+++ Pooja/GrandChild.java
@@ -0,0 +1,12 @@
+package MyPack;
+
+ class GrandChild extends Child1 {
+	 public static void main(String args[])
+	 {
+		 GrandChild obj=new GrandChild();
+		 obj.add();
+		 obj.add1();
+	 }
+	
+	
+}
