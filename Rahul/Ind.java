1a29f51f2a50b92b668c2d25d4e511d088eaf7b5
BTES17 <ramandeep@btes.co.in>
2017-04-20 09:56:11 +0530

    Use of Throws in exceptions
    import java.util.InputMismatchException;
    import java.util.Scanner;
    
    public class Ind {
    
    	public void Indi() throws InputMismatchException
    	{
    		int a;
    	Scanner scan=new Scanner (System.in);
    	System.out.println("Enter your age");
    	a=scan.nextInt();
    
    		}
    
    public static void main(String args[]) 
    {
    	Ind Obj=new Ind();
    	
    	try{
    		Obj.Indi();
    		
    		System.out.println("OK, thank you for entering your age");
    		}
    	
    	catch(InputMismatchException e)
    	{
    		System.out.println("Enter the age in numerical form please");
    	}
    	
    }
    }

--- Rahul/Ind.java
+++ Rahul/Ind.java
@@ -1,37 +1,31 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ind {
 
-	public void Indi() {
+	public void Indi() throws InputMismatchException
+	{
 		int a;
 	Scanner scan=new Scanner (System.in);
 	System.out.println("Enter your age");
-	
+	a=scan.nextInt();
+
+		}
+
+public static void main(String args[]) 
+{
+	Ind Obj=new Ind();
 	
 	try{
-		a=scan.nextInt();
+		Obj.Indi();
 		
 		System.out.println("OK, thank you for entering your age");
 		}
 	
-	/*catch(Exception e)
+	catch(InputMismatchException e)
 	{
 		System.out.println("Enter the age in numerical form please");
 	}
-	*/
-	finally{
-		
-		System.out.println("Thank you for submitting your age, we will validate it shortly.");
-	}
-	
-	System.out.println("checking if this prints or not.");
-	
-	}
-
-public static void main(String args[])
-{
-	Ind Obj=new Ind();
-	Obj.Indi();
 	
 }
 }
